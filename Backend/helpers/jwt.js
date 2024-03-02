7286518f0c45fcdbffa41ff289c6fccee19fe926
Helmi-Rezgui <helmiielrezgui@gmail.com>
2024-03-03 00:05:20 +0100

    adding user email verification

--- Backend/helpers/jwt.js
+++ Backend/helpers/jwt.js
@@ -14,7 +14,7 @@ function authJwt() {
       {url:  /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS']},
       `${api}/users/login`,
       `${api}/users/register`,
-      // `${api}/users/verify-email`
+      `${api}/users/verify-email`
     ]
   })
 }
